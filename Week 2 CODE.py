63ca7ff2d72ef9898b562b2cc4b91a1355290ed9
Upen Singh <110465937+upen122@users.noreply.github.com>
2025-06-14 10:31:25 +0530

    Update Week 2 CODE.py

--- Week 2 CODE.py
+++ Week 2 CODE.py
@@ -128,3 +128,98 @@ INPUT = AABCAAADA
 OUPUT = AB
 CA
 AD
+
+
+Task 6 - https://www.hackerrank.com/challenges/collections-counter/problem?isFullScreen=true
+
+from collections import Counter
+
+x = int(input())
+
+
+shoe_sizes = list(map(int, input().split()))
+shoe_inventory = Counter(shoe_sizes)  
+
+
+n = int(input())
+
+earnings = 0
+
+for _ in range(n):
+    size, price = map(int, input().split())
+    if shoe_inventory[size] > 0:
+        earnings += price
+        shoe_inventory[size] -= 1 
+
+print(earnings)
+
+INPUT = 10
+2 3 4 5 6 8 7 6 5 18
+6
+6 55
+6 45
+6 55
+4 40
+18 60
+10 50
+OUTPUT = 200
+
+
+Task 7 = https://www.hackerrank.com/challenges/exceptions/problem?isFullScreen=true
+
+t = int(input())
+for _ in range(t):
+    a, b = input().split()
+    try:
+        print(int(a) // int(b))
+    except (ZeroDivisionError, ValueError) as e:
+        print("Error Code:", e)
+
+INPUT = 3
+1 0
+2 $
+3 1
+OUTPUT = Error Code: integer division or modulo by zero
+Error Code: invalid literal for int() with base 10: '$'
+3
+
+
+Task 8 = https://www.hackerrank.com/challenges/incorrect-regex/problem?isFullScreen=true
+
+import re
+
+for _ in range(int(input())):
+    try:
+        re.compile(input())
+        print(True)
+    except re.error:
+        print(False)
+
+# NOTE CODE IS CORRECT BUT NOT RUNNING IN HARKERRANK 
+
+Task 9 = https://www.hackerrank.com/challenges/py-set-discard-remove-pop/problem?isFullScreen=true
+
+input()
+s = set(map(int, input().split()))
+for _ in range(int(input())):
+    cmd = input().split()
+    if cmd[0] == 'pop' and s:
+        s.pop()
+    elif cmd[0] in ('remove', 'discard') and len(cmd) == 2:
+        s.discard(int(cmd[1]))
+print(sum(s))
+
+INPUT = 9
+1 2 3 4 5 6 7 8 9
+10
+pop
+remove 9
+discard 9
+discard 8
+remove 7
+pop
+discard 6
+remove 5
+pop
+discard 5
+OUTPUT = 4
